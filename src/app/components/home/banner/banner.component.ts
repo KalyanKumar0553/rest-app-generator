7fd534853f018768324f6c3b1a83ac8b9eb49556
Kalyan Kumar <kalyankumar0553@gmail.com>
2025-11-23 22:53:05 +0530

    Updated home.component.ts

--- src/app/components/home/banner/banner.component.ts
+++ src/app/components/home/banner/banner.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-hero',
+  selector: 'app-banner',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
 })
-export class HeroComponent {}
+export class BannerComponent {}
