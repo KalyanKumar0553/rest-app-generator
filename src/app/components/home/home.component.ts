d3baba9d214798610c175b1da4e8ce4f1b792d43
Kalyan Kumar <kalyankumar0553@gmail.com>
2025-11-23 15:37:35 +0530

    Updated colors.css

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,34 +1,13 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../header/header.component';
-import { HeroComponent } from '../hero/hero.component';
-import { AboutComponent } from '../about/about.component';
-import { ServicesComponent } from '../services/services.component';
-import { TestimonialsComponent } from '../testimonials/testimonials.component';
-import { ContactComponent } from '../contact/contact.component';
-import { FooterComponent } from '../footer/footer.component';
-import { AnimationService } from '../../services/animation.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    CommonModule,
-    HeroComponent,
-    AboutComponent,
-    ServicesComponent,
-    TestimonialsComponent,
-    ContactComponent
-  ],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements AfterViewInit {
-
-  constructor(private animationService: AnimationService) {}
-
-  ngAfterViewInit(): void {
-    // Setup scroll animations after view initialization
-    this.animationService.setupScrollAnimations();
-  }
+export class HomeComponent {
+  constructor() {}
 }
