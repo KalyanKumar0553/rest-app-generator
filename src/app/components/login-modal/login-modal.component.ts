221c4898ed3a0857f2ed1cf74d628c54815f69ea
Kalyan Kumar <kalyankumar0553@gmail.com>
2025-11-25 10:56:56 +0530

    Updated app.routes.ts

--- src/app/components/login-modal/login-modal.component.ts
+++ src/app/components/login-modal/login-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-login-modal',
@@ -17,6 +19,12 @@ export class LoginModalComponent {
   password = '';
   acceptTerms = false;
   receiveUpdates = false;
+  emailError = '';
+
+  constructor(
+    private router: Router,
+    private modalService: ModalService
+  ) {}
 
   toggleMode(): void {
     this.isSignupMode = !this.isSignupMode;
@@ -28,6 +36,49 @@ export class LoginModalComponent {
     this.password = '';
     this.acceptTerms = false;
     this.receiveUpdates = false;
+    this.emailError = '';
+  }
+
+  validateEmail(): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!this.email) {
+      this.emailError = 'Email is required';
+      return false;
+    }
+    if (!emailPattern.test(this.email)) {
+      this.emailError = 'Please enter a valid email address';
+      return false;
+    }
+    this.emailError = '';
+    return true;
+  }
+
+  onEmailBlur(): void {
+    if (this.email) {
+      this.validateEmail();
+    }
+  }
+
+  isFormValid(): boolean {
+    if (!this.email || !this.password) {
+      return false;
+    }
+    if (this.isSignupMode && !this.acceptTerms) {
+      return false;
+    }
+    return this.emailError === '';
+  }
+
+  navigateToTerms(event: Event): void {
+    event.preventDefault();
+    this.modalService.closeLoginModal();
+    this.router.navigate(['/terms']);
+  }
+
+  navigateToPrivacy(event: Event): void {
+    event.preventDefault();
+    this.modalService.closeLoginModal();
+    this.router.navigate(['/privacy']);
   }
 
   closeModal(): void {
@@ -35,6 +86,10 @@ export class LoginModalComponent {
   }
 
   onSubmit(): void {
+    if (!this.validateEmail()) {
+      return;
+    }
+
     if (this.isSignupMode) {
       console.log('Signup:', { email: this.email, password: this.password, acceptTerms: this.acceptTerms, receiveUpdates: this.receiveUpdates });
     } else {
