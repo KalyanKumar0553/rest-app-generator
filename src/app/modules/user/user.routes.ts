67a0af5b38e03c712c8c0b9940f52b7f0be72b15
Kalyan Kumar <kalyankumar0553@gmail.com>
2025-11-26 18:21:11 +0530

    Added logout-response.json

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from '../../guards/auth.guard';
 
 export const USER_ROUTES: Routes = [
   {
     path: '',
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'dashboard',
