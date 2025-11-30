6a58628b5207e3c7bcb1cad6ab05c14dceada31e
Kalyan Kumar <kalyankumar0553@gmail.com>
2025-11-30 21:43:47 +0530

    Added add-entity.component.html

--- src/app/modules/project-generation/components/entities/entities.component.ts
+++ src/app/modules/project-generation/components/entities/entities.component.ts
@@ -1,18 +1,81 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddEntityComponent } from '../add-entity/add-entity.component';
+
+interface Entity {
+  name: string;
+  mappedSuperclass: boolean;
+  addRestEndpoints: boolean;
+  fields: any[];
+}
+
+interface Relation {
+  sourceEntity: string;
+  targetEntity: string;
+  relationType: string;
+  fieldName: string;
+}
 
 @Component({
   selector: 'app-entities',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AddEntityComponent],
   templateUrl: './entities.component.html',
   styleUrls: ['./entities.component.css']
 })
 export class EntitiesComponent {
-  @Input() entities: any[] = [];
-  @Input() relations: any[] = [];
+  @Input() entities: Entity[] = [];
+  @Input() relations: Relation[] = [];
 
-  addEntity() {
-    console.log('Add entity clicked');
+  showAddEntity = false;
+  entitiesExpanded = true;
+  relationsExpanded = true;
+  editingEntity?: Entity;
+
+  addEntity(): void {
+    this.editingEntity = undefined;
+    this.showAddEntity = true;
+  }
+
+  editEntity(entity: Entity): void {
+    this.editingEntity = entity;
+    this.showAddEntity = true;
+  }
+
+  deleteEntity(entity: Entity): void {
+    const index = this.entities.indexOf(entity);
+    if (index > -1) {
+      this.entities.splice(index, 1);
+    }
+  }
+
+  onEntitySave(entity: Entity): void {
+    if (this.editingEntity) {
+      const index = this.entities.indexOf(this.editingEntity);
+      if (index > -1) {
+        this.entities[index] = entity;
+      }
+    } else {
+      this.entities.push(entity);
+    }
+    this.showAddEntity = false;
+    this.editingEntity = undefined;
+  }
+
+  onEntityCancel(): void {
+    this.showAddEntity = false;
+    this.editingEntity = undefined;
+  }
+
+  toggleEntitiesPanel(): void {
+    this.entitiesExpanded = !this.entitiesExpanded;
+  }
+
+  toggleRelationsPanel(): void {
+    this.relationsExpanded = !this.relationsExpanded;
+  }
+
+  addRelation(): void {
+    console.log('Add relation clicked');
   }
 }
